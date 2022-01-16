c82b282b3f52b99d3119f62b66e40b52af68ac7a
Rahul S <rahuls@BVMDVSRAHUL.local>
2022-01-16 22:47:22 +0530

    Initial commit

---
+++ src/main/java/com/flock/TP_server/TpServerApplication.java
@@ -0,0 +1,13 @@
+package com.flock.TP_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flock/TP_server/TpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flock.TP_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
